3aade5712157ad79af5c30e6e9ce3876b2c3ff0e
Rolf Håvard Blindheim <rolf@moota.com>
2014-01-02 09:22:51 +0100

    fixed some urls
    Signed-off-by: Rolf Håvard Blindheim <rolf@moota.com>

--- apps/server/urls.py
+++ apps/server/urls.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import socketio.sdjango
+from django.conf.urls import patterns, include, url
+
+socketio.sdjango.autodiscover()
+
+urlpatterns = patterns(
+    "",
+    # Socket.IO urls
+    url("^socket\.io", include(socketio.sdjango.urls)),
+)
--- stickfight/urls.py
+++ stickfight/urls.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import socketio.sdjango
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
 
-socketio.sdjango.autodiscover()
 admin.autodiscover()
 
 urlpatterns = patterns(
     "",
     url(r"^account/admin/", include(admin.site.urls)),
-    url(r"^", include("apps.webclient.urls"))
-    # Socket.IO urls
-    # url("^socket\.io", include(socketio.sdjango.urls)),
+
+    # Include apps here
+    url(r"^", include("apps.webclient.urls")),  # We add the webclient at the root url
+    url(r"^server", include("apps.server.urls"))
 )
