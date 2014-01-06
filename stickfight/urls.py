df7dca849b742025acc111f50651ff7f8da6851e
Rolf Håvard Blindheim <rolf@moota.com>
2014-01-06 12:58:07 +0100

    added static content for debug
    Signed-off-by: Rolf Håvard Blindheim <rolf@moota.com>

--- stickfight/urls.py
+++ stickfight/urls.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from django.conf import settings
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 admin.autodiscover()
 
@@ -14,3 +16,6 @@ urlpatterns = patterns(
     url(r"^", include("apps.webclient.urls")),  # We add the webclient at the root url
     url(r"^server", include("apps.server.urls"))
 )
+
+if getattr(settings, "DEBUG", False) is True:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
