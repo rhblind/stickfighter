e52c246cf93ce3d74dba1e3ee7085d0146ad6847
Rolf Håvard Blindheim <rolf@moota.com>
2014-01-06 12:32:44 +0100

    some deployment stuff
    Signed-off-by: Rolf Håvard Blindheim <rolf@moota.com>

--- bin/gunicorn_start.sh
+++ bin/gunicorn_start.sh
@@ -5,20 +5,20 @@
 #
 
 
-NAME="mootadb"                                          # Name of the application
-DJANGODIR=/home/ubuntu/site/mootadb                     # Django project directory
-SOCKFILE=/var/run/gunicorn/mootadb.socket               # we will communicte using this unix socket
+NAME="stickfight"                                       # Name of the application
+DJANGODIR=/home/webapps/sites/stickfight                # Django project directory
+SOCKFILE=/var/run/gunicorn/stickfight.socket            # we will communicte using this unix socket
 USER=www-data                                           # the user to run as
 GROUP=www-data                                          # the group to run as
 NUM_WORKERS=3                                           # how many worker processes should Gunicorn spawn
-DJANGO_SETTINGS_MODULE=mootadb.settings                 # which settings file should Django use
-DJANGO_WSGI_MODULE=mootadb.wsgi                         # WSGI module name
+DJANGO_SETTINGS_MODULE=stickfight.settings              # which settings file should Django use
+DJANGO_WSGI_MODULE=stickfight.wsgi                      # WSGI module name
 
 echo "Starting ${NAME} as `whoami`"
 
 # Activate the virtual environment
 cd ${DJANGODIR}
-source ../env/bin/activate
+source ../../.virtualenvs/stickfight/bin/activate
 export DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE}
 export PYTHONPATH=${DJANGODIR}:${PYTHONPATH}
 
@@ -28,7 +28,7 @@ test -d ${RUNDIR} || mkdir -p ${RUNDIR}
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec ../env/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+exec ../../.virtualenvs/stickfight/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name ${NAME} \
   --workers ${NUM_WORKERS} \
   --user=${USER} --group=${GROUP} \
