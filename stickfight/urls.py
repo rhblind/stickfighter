6e00a094f0b02c50ad7e45684d408563b76dcdf7
Rolf Håvard Blindheim <rolf@moota.com>
2014-01-13 10:08:47 +0100

    blah
    Signed-off-by: Rolf Håvard Blindheim <rolf@moota.com>

---
+++ stickfight/urls.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from django.conf import settings
+from django.contrib import admin
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+admin.autodiscover()
+
+urlpatterns = patterns(
+    "",
+    url(r"^accounts/", include("allauth.urls")),
+    url(r"^accounts/admin/", include(admin.site.urls)),
+
+    # Include apps here
+    # url(r"^", include("apps.webclient.urls")),  # We add the webclient at the root url
+    url(r"^", TemplateView.as_view(template_name="index.html")),
+    url(r"^server/", include("apps.server.urls"))
+)
+
+if getattr(settings, "DEBUG", False) is True:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
